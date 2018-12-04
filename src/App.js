7034fd46c4c049c6f2ec0dc5df7c0a988a555556
Ritvik Vasudevan <ritvikvasudevan@Ritviks-MacBook-Pro.local>
2018-12-04 13:12:27 -0500

    cleaned up substantially

--- src/App.js
+++ src/App.js
@@ -23,9 +23,33 @@ class App extends Component {
       velocity :0, 
       t0: new Date(),
       driving_state: "flat", 
-      background_image : background_0
+      background_image : background_0,
+      show_debugger: false,
+      button_name: "Show Debugger",
+      image_visibility: "visible",
+      absolute: 0,
+      alpha: 0,
+      beta: 0,
+      gamma: 0
     }
     this.handleMotion = this.handleMotion.bind(this)
+    this.handleOrientation = this.handleOrientation.bind(this)
+    this.handleDebugger = this.handleDebugger.bind(this);
+
+    this.debugger = (
+      <div className = "App">
+        <b>Acceleration</b><br/><div>
+        a_x: <div className = "values">{this.state.x}</div><br/>
+        </div>
+        a_y: {this.state.y}<br/>
+        a_z: {this.state.z}<br/>
+        <br/>
+        <b>Device Rotation</b><br/>
+        alpha: {this.state.alpha}<br/>
+        beta: {this.state.beta}<br/>
+        gamma: {this.state.gamma}<br/>
+      </div>
+    )
   }
   componentDidMount(){
     window.addEventListener('devicemotion',this.handleMotion)
@@ -44,10 +68,35 @@ class App extends Component {
     }
   }
   handleOrientation(event){
-    console.log("yo")
+    this.setState({
+      absolute: event.absolute,
+      alpha: event.alpha,
+      beta: event.beta,
+      gamma: event.gamma
+    })
+  }
+  handleDebugger(event){
+    if (this.state.show_debugger == false){
+      this.setState({
+        show_debugger: true,
+        image_visibility: "hidden",
+        button_name: "Hide Debugger"
+      })
+    }
+    else {
+      this.setState({
+        show_debugger: false,
+        image_visibility: "visible",
+        button_name: "Show Debugger"
+      })
+    }
   }
   total_accel(x, y, z){
-    return Math.sqrt(x*x+y*y+z*z)
+    var direction = 1.0;
+    if (x<0){
+      direction = -1.0;
+    }
+    return direction*Math.sqrt(x*x+y*y+z*z)
   }
   read_image(accel){
     // var hard_braking = -5.3936575
@@ -65,13 +114,35 @@ class App extends Component {
     }
   }
   render() {
+    var image_style = {
+      visibility :this.state.image_visibility
+    }
+    var style = {
+      zIndex: "10"
+    }
+    var button_style = {
+      color: "grey",
+      fontColor: "white"
+    }
+    if (!this.state.show_debugger) {
+      style = {
+        display:'none'
+      }
+    }
     return (
       <div>
         <meta name = "apple-mobile-web-app-capable" content="yes" />
           <div className="App">
             <header className="App-header">
               {/* <img src ={logo} className = "App-logo"></img> */}
-              <img src={this.state.background_image} className="Background-Image" alt="logo" />
+              <div className = "container">
+                <button type = "Button" onClick = {this.handleDebugger} className = "btn" >{this.state.button_name}</button>
+                <img src={this.state.background_image} className="Background-Image" alt="logo" style = {image_style}/>
+                <div className = "Debugger" style = {style}>
+                  {this.debugger}
+                </div>
+              </div>
+              
             </header>
           </div>
       </div>
